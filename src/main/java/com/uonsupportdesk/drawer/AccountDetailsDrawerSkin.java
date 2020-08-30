4d46fbb7b649f57c7398744395b96b64240bff2e
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-08-31 00:33:05 +0100

    Remove redundant import

--- src/main/java/com/uonsupportdesk/drawer/AccountDetailsDrawerSkin.java
+++ src/main/java/com/uonsupportdesk/drawer/AccountDetailsDrawerSkin.java
@@ -8,7 +8,6 @@ import javafx.scene.control.SkinBase;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.util.Objects;
