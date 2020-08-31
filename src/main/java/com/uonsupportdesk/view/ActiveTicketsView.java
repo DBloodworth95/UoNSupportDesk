b418f1050d69c58a6be70b9a8149e25dbaa73633
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-08-31 13:31:37 +0100

    Add content to scroll windows

--- src/main/java/com/uonsupportdesk/view/ActiveTicketsView.java
+++ src/main/java/com/uonsupportdesk/view/ActiveTicketsView.java
@@ -1,18 +1,29 @@
 package com.uonsupportdesk.view;
 
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
 
 public class ActiveTicketsView extends BorderPane {
 
     private ScrollPane activeTicketsListScroll;
     private ScrollPane activeChatScroll;
+    private Pane activeTicketsContent;
+    private Pane activeChatContent;
+    private Label noActiveTicketsLabel;
+    private Label noChatOpenLabel;
 
     private static final int ACTIVE_TICKET_LIST_WIDTH = 300;
 
     public ActiveTicketsView() {
         activeTicketsListScroll = new ScrollPane();
         activeChatScroll = new ScrollPane();
+        activeChatContent = new Pane();
+        activeTicketsContent = new Pane();
+        noActiveTicketsLabel = new Label("No tickets available");
+        noChatOpenLabel = new Label("Select a ticket");
 
         activeChatScroll.prefViewportHeightProperty().bind(this.heightProperty());
         activeChatScroll.prefViewportWidthProperty().bind(this.widthProperty());
@@ -21,5 +32,14 @@ public class ActiveTicketsView extends BorderPane {
 
         this.setLeft(activeTicketsListScroll);
         this.setRight(activeChatScroll);
+
+        activeChatScroll.setContent(activeChatContent);
+        activeTicketsListScroll.setContent(activeTicketsContent);
+
+        activeChatContent.getChildren().add(noChatOpenLabel);
+        activeTicketsContent.getChildren().add(noActiveTicketsLabel);
+
+        noChatOpenLabel.setAlignment(Pos.BASELINE_CENTER);
+        noActiveTicketsLabel.setAlignment(Pos.BASELINE_CENTER);
     }
 }
