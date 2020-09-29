3a9c538f41f4df5372485cc4b4c5f76ddb3adfcc
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-09-29 16:50:29 +0100

    Add content to side bar & bottom window

--- src/main/java/com/uonsupportdesk/view/UnassignedTicketsView.java
+++ src/main/java/com/uonsupportdesk/view/UnassignedTicketsView.java
@@ -1,9 +1,15 @@
 package com.uonsupportdesk.view;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
+
 
 public class UnassignedTicketsView extends AnchorPane {
 
@@ -17,6 +23,8 @@ public class UnassignedTicketsView extends AnchorPane {
 
     private final Label activeTicketsTitleLabel;
 
+    private final Label totalTicketsLabel;
+
     private final JFXButton searchButton;
 
     private final JFXButton helpButton;
@@ -24,14 +32,28 @@ public class UnassignedTicketsView extends AnchorPane {
     private final JFXButton settingsButton;
 
     public UnassignedTicketsView() {
+        Image settingsIcon = new Image(getClass().getResourceAsStream("/icons/settings_icon.png"));
+        Image helpIcon = new Image(getClass().getResourceAsStream("/icons/help-icon.png"));
+        ImageView helpIconView = new ImageView(helpIcon);
+        ImageView settingsIconView = new ImageView(settingsIcon);
+
         listOfTicketsScrollPane = new ScrollPane();
         listOfTicketsContainer = new VBox();
         sideBarContainer = new VBox();
         totalTicketsContainer = new VBox();
         activeTicketsTitleLabel = new Label("Active Tickets");
+        totalTicketsLabel = new Label("Total Tickets: ");
         searchButton = new JFXButton("Search");
-        helpButton = new JFXButton("Help");
-        settingsButton = new JFXButton("Settings");
+        helpButton = new JFXButton("Help", helpIconView);
+        settingsButton = new JFXButton("Settings", settingsIconView);
+
+        settingsButton.setPrefWidth(200);
+        settingsButton.setGraphicTextGap(5);
+        helpButton.setGraphicTextGap(5);
+        helpButton.setPrefWidth(200);
+
+        totalTicketsLabel.setFont(new Font(20));
+        activeTicketsTitleLabel.setFont(new Font(24));
 
         settingsButton.getStyleClass().add("unassigned-ticket-buttons");
         searchButton.getStyleClass().add("unassigned-ticket-buttons");
@@ -54,20 +76,29 @@ public class UnassignedTicketsView extends AnchorPane {
         sideBarContainer.prefWidthProperty().bind(this.widthProperty().multiply(0.2));
         listOfTicketsScrollPane.prefWidthProperty().bind(this.widthProperty().subtract(sideBarContainer.widthProperty()));
         totalTicketsContainer.prefHeightProperty().bind(this.heightProperty().subtract(listOfTicketsScrollPane.heightProperty()));
-        totalTicketsContainer.prefWidthProperty().bind(listOfTicketsScrollPane.widthProperty());
+        totalTicketsContainer.prefWidthProperty().bind(listOfTicketsScrollPane.widthProperty().subtract(200));
 
         getChildren().addAll(sideBarContainer, listOfTicketsScrollPane, totalTicketsContainer);
         setTopAnchor(sideBarContainer, 0.0);
         setTopAnchor(listOfTicketsScrollPane, 0.0);
         setLeftAnchor(sideBarContainer, 0.0);
         setRightAnchor(listOfTicketsScrollPane, 0.0);
-        setRightAnchor(totalTicketsContainer, 0.0);
+        setRightAnchor(totalTicketsContainer, 100.0);
         setBottomAnchor(totalTicketsContainer, 0.0);
         setBottomAnchor(sideBarContainer, 0.0);
+
+        sideBarContainer.setAlignment(Pos.CENTER);
+        totalTicketsContainer.setAlignment(Pos.TOP_LEFT);
+
+        VBox.setMargin(activeTicketsTitleLabel, new Insets(0, 0, 50, 0));
+        VBox.setMargin(helpButton, new Insets(0, 0, 250, 0));
+        VBox.setMargin(settingsButton, new Insets(400, 0, 0, 0));
+        totalTicketsContainer.setPadding(new Insets(30, 0, 0, 0));
     }
 
     private void addContentToWindows() {
-
+        sideBarContainer.getChildren().addAll(activeTicketsTitleLabel, helpButton, settingsButton);
+        totalTicketsContainer.getChildren().add(totalTicketsLabel);
     }
 
     private void attachListeners() {
