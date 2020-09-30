ebc609fa69234d75633807f8f9ab313282c8b85c
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-09-30 11:53:02 +0100

    Remove redundant imports

--- src/main/java/com/uonsupportdesk/module/component/UnassignedTicketWidget.java
+++ src/main/java/com/uonsupportdesk/module/component/UnassignedTicketWidget.java
@@ -1,7 +1,6 @@
 package com.uonsupportdesk.module.component;
 
 import com.jfoenix.controls.JFXButton;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
