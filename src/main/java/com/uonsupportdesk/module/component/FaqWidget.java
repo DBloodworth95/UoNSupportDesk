970e642635891252a133946278a6e6f14c1877f8
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-07 11:48:02 +0100

    Finish FAQ Module

---
+++ src/main/java/com/uonsupportdesk/module/component/FaqWidget.java
@@ -0,0 +1,42 @@
+package com.uonsupportdesk.module.component;
+
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class FaqWidget extends VBox {
+
+    private final Label titleLabel;
+
+    private final Label questionOneLabel;
+
+    private final Label questionTwoLabel;
+
+    private final Label questionThreeLabel;
+
+    public FaqWidget(String title, String question1, String question2, String question3) {
+        this.setSpacing(10);
+
+        titleLabel = new Label(title);
+        questionOneLabel = new Label(question1);
+        questionTwoLabel = new Label(question2);
+        questionThreeLabel = new Label(question3);
+
+        titleLabel.getStyleClass().add("faq-widget-header");
+        questionOneLabel.getStyleClass().add("faq-widget-question");
+        questionTwoLabel.getStyleClass().add("faq-widget-question");
+        questionThreeLabel.getStyleClass().add("faq-widget-question");
+
+        positionComponents();
+        addContentToWindow();
+    }
+
+    private void positionComponents() {
+        titleLabel.setPadding(new Insets(0, 0, 10, 0));
+
+    }
+
+    private void addContentToWindow() {
+        getChildren().addAll(titleLabel, questionOneLabel, questionTwoLabel, questionThreeLabel);
+    }
+}
--- src/main/java/com/uonsupportdesk/view/FaqView.java
+++ src/main/java/com/uonsupportdesk/view/FaqView.java
@@ -1,12 +1,16 @@
 package com.uonsupportdesk.view;
 
+import com.uonsupportdesk.module.component.FaqWidget;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+
+import java.util.stream.DoubleStream;
 
 public class FaqView extends BorderPane {
 
@@ -22,6 +26,19 @@ public class FaqView extends BorderPane {
 
     private final TextField faqTopicSearchTextField;
 
+    private final FaqWidget generalWidget;
+
+    private final FaqWidget financeWidget;
+
+    private final FaqWidget accommodationWidget;
+
+    private final FaqWidget courseWidget;
+
+    private final FaqWidget securityWidget;
+
+    private final FaqWidget otherWidget;
+
+
     public FaqView() {
         headerContainer = new VBox();
         faqContentContainer = new GridPane();
@@ -30,12 +47,19 @@ public class FaqView extends BorderPane {
         faqContentHeaderLabel = new Label("Frequently Asked Questions");
         faqTopicSearchTextField = new TextField();
         faqTopicSearchTextField.setPromptText("Search keywords here");
+        generalWidget = new FaqWidget("General", "Question 1", "Question 2", "Question 3");
+        financeWidget = new FaqWidget("Finance", "Question 1", "Question 2", "Question 3");
+        accommodationWidget = new FaqWidget("Accommodation", "Question 1", "Question 2", "Question 3");
+        courseWidget = new FaqWidget("Courses", "Question 1", "Question 2", "Question 3");
+        securityWidget = new FaqWidget("Security", "Question 1", "Question 2", "Question 3");
+        otherWidget = new FaqWidget("Other", "Question 1", "Question 2", "Question 3");
 
         headerContainer.getStyleClass().add("header-container");
         faqContentContainer.getStyleClass().add("faq-header-container");
         faqTopicSearchTextField.getStyleClass().add("faq-search-field");
         headerLabel.getStyleClass().add("faq-header");
         subheaderLabel.getStyleClass().add("faq-subheader");
+        faqContentHeaderLabel.getStyleClass().add("faq-content-header");
 
         positionComponents();
         addContentToWindows();
@@ -53,12 +77,30 @@ public class FaqView extends BorderPane {
         headerContainer.setSpacing(50);
         headerLabel.setPadding(new Insets(20, 0, 0, 0));
         subheaderLabel.setPadding(new Insets(0, 0, 20, 0));
+        faqContentContainer.setPadding(new Insets(25, 20, 0, 20));
 
         faqTopicSearchTextField.setMinHeight(50);
         faqTopicSearchTextField.setMaxWidth(800);
+
+
+        faqContentContainer.setVgap(75);
+        faqContentContainer.getColumnConstraints().addAll(DoubleStream.of(50, 50, 50)
+                .mapToObj(width -> {
+                    ColumnConstraints constraints = new ColumnConstraints();
+                    constraints.setPercentWidth(width);
+                    constraints.setFillWidth(true);
+                    return constraints;
+                }).toArray(ColumnConstraints[]::new));
     }
 
     private void addContentToWindows() {
+        faqContentContainer.add(faqContentHeaderLabel, 0, 0, 2, 1);
+        faqContentContainer.add(generalWidget, 0, 1);
+        faqContentContainer.add(financeWidget, 1, 1);
+        faqContentContainer.add(accommodationWidget, 2, 1);
+        faqContentContainer.add(courseWidget, 0, 2);
+        faqContentContainer.add(securityWidget, 1, 2);
+        faqContentContainer.add(otherWidget, 2, 2);
         headerContainer.getChildren().addAll(headerLabel, faqTopicSearchTextField, subheaderLabel);
     }
 
