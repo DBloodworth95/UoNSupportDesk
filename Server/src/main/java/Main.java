ac47feeef99b090bc67d2c9461b9c7ed3ce376ca
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-19 17:48:00 +0100

    Add option for multi-threaded socket handling

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Server server = new Server();
+        Server server = new Server(true);
         Thread serverThread = new Thread(server);
         serverThread.start();
     }
--- Server/src/main/java/Server.java
+++ Server/src/main/java/Server.java
@@ -3,11 +3,22 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server implements Runnable {
     private static final int PORT = 8818;
 
     private final List<ConnectionHelper> connections = new ArrayList<>();
+
+    private final ExecutorService connectionHelperService;
+
+    public Server(boolean isMultiThreaded) {
+        if (isMultiThreaded)
+            this.connectionHelperService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        else
+            this.connectionHelperService = Executors.newFixedThreadPool(1);
+    }
 
     @SuppressWarnings("InfiniteLoopStatement")
     @Override
