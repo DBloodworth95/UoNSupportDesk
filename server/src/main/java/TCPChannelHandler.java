21ed3aa3d22c1bf906421dd09b2e4429aebe86de
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-21 12:16:00 +0100

    Format

--- server/src/main/java/TCPChannelHandler.java
+++ server/src/main/java/TCPChannelHandler.java
@@ -7,6 +7,7 @@ public class TCPChannelHandler extends SimpleChannelInboundHandler<String> {
     public void channelActive(ChannelHandlerContext ctx) {
         System.out.println(ctx.channel().remoteAddress() + " Channel Active");
     }
+
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
         ctx.channel().writeAndFlush("Thank you for the message!");
