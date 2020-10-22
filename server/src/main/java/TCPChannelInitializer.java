d64b3f837b7d67b6dbecfca28b46e62c732144dd
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-22 22:49:37 +0100

    Add server encoding/decoding

--- server/src/main/java/TCPChannelInitializer.java
+++ server/src/main/java/TCPChannelInitializer.java
@@ -1,5 +1,7 @@
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
 import io.netty.handler.codec.string.StringDecoder;
 import io.netty.handler.codec.string.StringEncoder;
 
@@ -7,8 +9,12 @@ public class TCPChannelInitializer extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel socketChannel) {
-        socketChannel.pipeline().addLast(new StringEncoder())
+        socketChannel
+                .pipeline() // downstream V
+                .addLast(new LengthFieldBasedFrameDecoder(Short.MAX_VALUE, 0, 2, 0, 2))
                 .addLast(new StringDecoder())
+                .addLast(new LengthFieldPrepender(2, false))
+                .addLast(new StringEncoder()) // upstream ^
                 .addLast(new TCPChannelHandler());
     }
 }
