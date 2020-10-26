167025a6968ca3cdeaf271d972976cd9948c559e
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-26 17:29:52 +0000

    Add message service

---
+++ server/src/main/java/service/MessageService.java
@@ -0,0 +1,10 @@
+package service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class MessageService implements Service {
+    @Override
+    public void submit(JsonNode jsonNode) {
+
+    }
+}
