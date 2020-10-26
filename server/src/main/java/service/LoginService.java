700a32502ddb552290a3eab6aa20fb36b18ca923
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-26 17:29:32 +0000

    Add login service

---
+++ server/src/main/java/service/LoginService.java
@@ -0,0 +1,10 @@
+package service;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class LoginService implements Service {
+    @Override
+    public void submit(JsonNode jsonNode) {
+
+    }
+}
