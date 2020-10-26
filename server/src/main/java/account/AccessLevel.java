47eb48d80cbaa2aa98db9bb47031d9fbda4c50f3
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-26 22:17:33 +0000

    Add account to server-side model

---
+++ server/src/main/java/account/AccessLevel.java
@@ -0,0 +1,13 @@
+package account;
+
+public enum AccessLevel {
+    SUPPORT_TEAM, USER;
+
+    public static AccessLevel fromInt(int id) {
+        switch (id) {
+            case 1: return SUPPORT_TEAM;
+            case 2: return USER;
+            default: throw new IllegalArgumentException("Invalid ID");
+        }
+    }
+}
