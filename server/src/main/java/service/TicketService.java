3661afabd89d6c96a4fb89192c9798453473baba
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-10-29 17:45:51 +0000

    Remove redundant import

--- server/src/main/java/service/TicketService.java
+++ server/src/main/java/service/TicketService.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import command.AcademicTicketRequestAccepted;
-import command.LoginRequestAccepted;
 import repository.AcademicTicketRepository;
 import ticket.AcademicTicket;
 
