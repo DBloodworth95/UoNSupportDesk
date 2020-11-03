882ae6624af1916f90f4162841c5dd89f8e993f0
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-03 19:04:05 +0000

    Add conversation repository

---
+++ server/src/main/java/repository/ConversationRepository.java
@@ -0,0 +1,41 @@
+package repository;
+
+import conversation.InitialConversation;
+
+import java.sql.*;
+
+public class ConversationRepository implements Repository {
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/uonsuport-schema";
+
+    private static final String DATABASE_USERNAME = "root";
+
+    private static final String DATABASE_PASSWORD = "root";
+
+    private static final String CREATE_CONVERSATION_QUERY = "INSERT INTO conversations (ticket_id) VALUES (?)";
+
+    public static InitialConversation submit(int ticketId) {
+        InitialConversation initialConversation = null;
+        int conversationId = 0;
+
+        try {
+            Connection connection = DriverManager.getConnection(DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD);
+            PreparedStatement preparedStatement = connection.prepareStatement(CREATE_CONVERSATION_QUERY, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setInt(1, ticketId);
+            preparedStatement.execute();
+
+            ResultSet resultSet = preparedStatement.getGeneratedKeys();
+            while (resultSet.next()) {
+                conversationId = resultSet.getInt(1);
+            }
+
+            initialConversation = new InitialConversation(ticketId, conversationId);
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return initialConversation;
+    }
+}
