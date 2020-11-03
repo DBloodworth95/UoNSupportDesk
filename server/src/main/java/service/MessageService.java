03f2dc8ceba142891e8d4bf8e6b9c9d7129ac070
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-03 19:03:40 +0000

    Add conversation handling

--- server/src/main/java/service/MessageService.java
+++ server/src/main/java/service/MessageService.java
@@ -1,12 +1,51 @@
 package service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import command.InitialConversationSuccess;
+import conversation.InitialConversation;
+import repository.ConversationRepository;
 
 public class MessageService implements Service {
 
     private final ObjectMapper responseMapper = new ObjectMapper();
 
-    public void submitConversation(String ticketDetails) {
+    public String submitConversation(String ticketDetails) {
+        InitialConversation initialConversation = null;
+        JsonNode ticketDetailsAsJson;
 
+        try {
+            ticketDetailsAsJson = responseMapper.readTree(ticketDetails);
+            int ticketId = ticketDetailsAsJson.get("ticketId").asInt();
+
+            initialConversation = ConversationRepository.submit(ticketId);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        if (initialConversation == null) return generateFailedResponse();
+
+        String response = generateConversationStartedResponse(initialConversation.getTicketId(), initialConversation.getConversationId());
+        if (response == null) return generateFailedResponse();
+
+        return response;
+    }
+
+    private String generateFailedResponse() {
+        return "{\"response\":\"ticketrequestfailed\"}";
+    }
+
+    private String generateConversationStartedResponse(int ticketId, int conversationId) {
+        String responseAsString = null;
+        InitialConversationSuccess initialConversationSuccess = new InitialConversationSuccess(ticketId, conversationId);
+
+        try {
+            responseAsString = responseMapper.writeValueAsString(initialConversationSuccess);
+        } catch (JsonProcessingException ignored) {
+
+        }
+
+        return responseAsString;
     }
 }
