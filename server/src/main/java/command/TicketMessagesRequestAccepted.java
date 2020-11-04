85c573e56ecb671e3969c7591ea0af4f2d030e65
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-04 14:51:31 +0000

    Include server-side successful ticket message fetching request

---
+++ server/src/main/java/command/TicketMessagesRequestAccepted.java
@@ -0,0 +1,39 @@
+package command;
+
+import conversation.Message;
+
+import java.util.List;
+
+
+public class TicketMessagesRequestAccepted implements Command {
+    private final String response;
+
+    private final int ticketId;
+
+    private final String ticketType;
+
+    private final List<Message> messages;
+
+    public TicketMessagesRequestAccepted(int ticketId, String ticketType, List<Message> messages) {
+        this.response = "getticketssuccess";
+        this.ticketId = ticketId;
+        this.ticketType = ticketType;
+        this.messages = messages;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public String getTicketType() {
+        return ticketType;
+    }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+}
