9c81ace74332503e4d99bc658a3cc05847930826
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-04 14:51:04 +0000

    Include ticket message fetching request handling to message service

--- server/src/main/java/service/MessageService.java
+++ server/src/main/java/service/MessageService.java
@@ -3,7 +3,9 @@ package service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import command.TicketMessagesRequestAccepted;
 import conversation.InitialConversation;
+import conversation.MessageList;
 import repository.MessageRepository;
 
 public class MessageService implements Service {
@@ -27,6 +29,33 @@ public class MessageService implements Service {
 
         String response = null;
         if (response == null) return generateFailedResponse();
+
+        return response;
+    }
+
+    public String getTicketMessages(JsonNode ticketDetails) {
+        int ticketId = ticketDetails.get("ticketId").asInt();
+        String ticketType = ticketDetails.get("ticketType").asText();
+
+        MessageList messageList = MessageRepository.findAll(ticketId, ticketType);
+        if (messageList == null) return generateFailedResponse();
+
+        String response = generateSuccessResponse(messageList);
+        if (response == null) return generateFailedResponse();
+
+        return response;
+    }
+
+    private String generateSuccessResponse(MessageList messageList) {
+        TicketMessagesRequestAccepted ticketMessagesRequestAccepted = new TicketMessagesRequestAccepted(messageList.getTicketId(), messageList.getTicketType(),
+                messageList.getMessages());
+        String response = null;
+
+        try {
+            response = responseMapper.writeValueAsString(ticketMessagesRequestAccepted);
+        } catch (JsonProcessingException ignored) {
+
+        }
 
         return response;
     }
