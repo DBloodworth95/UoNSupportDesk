903802cb794b31a81e0671dcc302501d235d5579
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-05 21:15:49 +0000

    Add message handling to message service

--- server/src/main/java/service/MessageService.java
+++ server/src/main/java/service/MessageService.java
@@ -3,31 +3,58 @@ package service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import command.MessageSubmitRequestAccepted;
 import command.TicketMessagesRequestAccepted;
-import conversation.InitialConversation;
+import conversation.Message;
 import conversation.MessageList;
 import repository.MessageRepository;
+import repository.UserTicketRepository;
 
 public class MessageService implements Service {
 
     private final ObjectMapper responseMapper = new ObjectMapper();
 
+    public int getParticipant(String ticketDetails) {
+        int participantId = 0;
+
+        try {
+            JsonNode ticketDetailsAsJson = responseMapper.readTree(ticketDetails);
+            int ticketId = ticketDetailsAsJson.get("ticketId").asInt();
+            String ticketType = ticketDetailsAsJson.get("ticketType").asText();
+
+            if (ticketType.equalsIgnoreCase("academic")) {
+                participantId = UserTicketRepository.getParticipantOfAcademicTicket(ticketId);
+            } else if (ticketType.equalsIgnoreCase("it")) {
+                participantId = UserTicketRepository.getParticipantOfTechnicalTicket(ticketId);
+            }
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        return participantId;
+    }
+
     public String submitMessage(String ticketDetails) {
-        InitialConversation initialConversation = null;
+        Message message = null;
         JsonNode ticketDetailsAsJson;
 
         try {
             ticketDetailsAsJson = responseMapper.readTree(ticketDetails);
             int ticketId = ticketDetailsAsJson.get("ticketId").asInt();
+            String ticketType = ticketDetailsAsJson.get("ticketType").asText();
+            String body = ticketDetailsAsJson.get("messageBody").asText();
+            String timestamp = ticketDetailsAsJson.get("timestamp").asText();
+            int authorId = ticketDetailsAsJson.get("authorId").asInt();
 
-            initialConversation = MessageRepository.submit(ticketId);
+
+            message = MessageRepository.submit(ticketId, ticketType, body, timestamp, authorId);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
 
-        if (initialConversation == null) return generateFailedResponse();
+        if (message == null) return generateFailedResponse();
 
-        String response = null;
+        String response = generateSuccessMessageResponse(message);
         if (response == null) return generateFailedResponse();
 
         return response;
@@ -42,6 +69,20 @@ public class MessageService implements Service {
 
         String response = generateSuccessResponse(messageList);
         if (response == null) return generateFailedResponse();
+
+        return response;
+    }
+
+    private String generateSuccessMessageResponse(Message message) {
+        MessageSubmitRequestAccepted messageSubmitRequestAccepted = new MessageSubmitRequestAccepted(message.getTicketId(), message.getTicketType(),
+                message.getMessage(), message.getTimestamp(), message.getAuthorId());
+        String response = null;
+
+        try {
+            response = responseMapper.writeValueAsString(messageSubmitRequestAccepted);
+        } catch (JsonProcessingException ignored) {
+
+        }
 
         return response;
     }
@@ -63,5 +104,4 @@ public class MessageService implements Service {
     private String generateFailedResponse() {
         return "{\"response\":\"ticketrequestfailed\"}";
     }
-
 }
