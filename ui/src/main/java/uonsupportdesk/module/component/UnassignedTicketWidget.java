ccde0f19d67dd379b3ab074f0ade74c29d8b47ce
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2020-11-09 21:33:30 +0000

    Optimizes ticket widget styling

--- ui/src/main/java/uonsupportdesk/module/component/UnassignedTicketWidget.java
+++ ui/src/main/java/uonsupportdesk/module/component/UnassignedTicketWidget.java
@@ -10,10 +10,13 @@ import javafx.scene.layout.Region;
 import javafx.scene.text.TextAlignment;
 
 public class UnassignedTicketWidget extends HBox {
+    private final int ticketId;
+
+    private final String name;
+
+    private final String description;
 
     private final String ticketType;
-
-    private final Label ticketIDLabel;
 
     private final Label nameOfUserLabel;
 
@@ -21,27 +24,27 @@ public class UnassignedTicketWidget extends HBox {
 
     private final JFXButton assignTicketButton;
 
-    private final Region idToNameSpacer;
-
     private final Region nameToDescriptionSpacer;
 
     private final Region descriptionToAssignSpacer;
 
-    public UnassignedTicketWidget(int ticketId, String nameOfUser, String ticketReason, String ticketType, boolean isAlternate) {
+    public UnassignedTicketWidget(int ticketId, String name, String description, String ticketType, boolean isAlternate) {
+        this.ticketId = ticketId;
+        this.name = name;
+        this.description = description;
         this.ticketType = ticketType;
-        if (isAlternate)
-            this.getStyleClass().add("unassigned-ticket-widget");
 
-        ticketIDLabel = new Label(String.valueOf(ticketId));
-        nameOfUserLabel = new Label(nameOfUser);
-        ticketReasonLabel = new Label(ticketReason);
+        if (isAlternate) {
+            this.getStyleClass().add("unassigned-ticket-widget");
+        }
+
+        nameOfUserLabel = new Label(name);
+        ticketReasonLabel = new Label(description);
         assignTicketButton = new JFXButton("Assign");
-        idToNameSpacer = new Region();
         nameToDescriptionSpacer = new Region();
         descriptionToAssignSpacer = new Region();
         assignTicketButton.getStyleClass().add("unassigned-assign-ticket-button");
 
-        HBox.setHgrow(idToNameSpacer, Priority.SOMETIMES);
         HBox.setHgrow(nameToDescriptionSpacer, Priority.SOMETIMES);
         HBox.setHgrow(descriptionToAssignSpacer, Priority.SOMETIMES);
 
@@ -52,17 +55,31 @@ public class UnassignedTicketWidget extends HBox {
     private void defineComponentLayout() {
         this.setPadding(new Insets(20, 50, 20, 50));
         this.setAlignment(Pos.CENTER_LEFT);
-        ticketIDLabel.setTextAlignment(TextAlignment.LEFT);
+        nameOfUserLabel.setAlignment(Pos.CENTER_LEFT);
+        ticketReasonLabel.setAlignment(Pos.CENTER_LEFT);
         nameOfUserLabel.setTextAlignment(TextAlignment.LEFT);
         ticketReasonLabel.setTextAlignment(TextAlignment.LEFT);
+
         nameOfUserLabel.setMinWidth(100);
-        ticketReasonLabel.setMinWidth(100);
+        assignTicketButton.setMinWidth(100);
     }
 
     private void addComponents() {
         this.getChildren().addAll(nameOfUserLabel, nameToDescriptionSpacer,
                 ticketReasonLabel, descriptionToAssignSpacer,
                 assignTicketButton);
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getTicketType() {
