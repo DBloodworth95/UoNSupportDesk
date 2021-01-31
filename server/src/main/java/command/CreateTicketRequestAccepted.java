1d47f4829e6dfd8c280451a47c9d3f91b45e892b
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2021-01-31 13:34:15 +0000

    Include ticket description and author's name in successful ticket request response

--- server/src/main/java/command/CreateTicketRequestAccepted.java
+++ server/src/main/java/command/CreateTicketRequestAccepted.java
@@ -5,15 +5,29 @@ public class CreateTicketRequestAccepted implements Command {
 
     private final String enquiryType;
 
+    private final String description;
+
+    private final String name;
+
     private final int userId;
 
     private final int ticketId;
 
-    public CreateTicketRequestAccepted(int userId, String enquiryType, int ticketId) {
+    public CreateTicketRequestAccepted(int userId, String enquiryType, int ticketId, String description, String name) {
         this.response = "createticketsuccess";
         this.userId = userId;
         this.enquiryType = enquiryType;
         this.ticketId = ticketId;
+        this.description = description;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getResponse() {
