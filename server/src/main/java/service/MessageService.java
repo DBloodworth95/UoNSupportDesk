3ff08d9b4414112111e222fc65866bfc3d0e98ba
dan bloodworth <daniel.bloodworth18@my.northampton.ac.uk>
2021-05-17 11:14:07 +0100

    Remove experimental serialisation strategy handling

--- server/src/main/java/service/MessageService.java
+++ server/src/main/java/service/MessageService.java
@@ -7,15 +7,9 @@ import command.MessageSubmitRequestAccepted;
 import command.TicketMessagesRequestAccepted;
 import conversation.Message;
 import conversation.MessageList;
-import javaobject.MessageSubmitRequestDenied;
-import javaobject.ObjectCommand;
-import javaobject.SubmitMessageRequest;
-import protobuf.ProtoMessageBuffer;
 import repository.AccountRepository;
 import repository.MessageRepository;
 import repository.UserTicketRepository;
-import utils.ObjectDeserializer;
-import utils.ObjectSerializer;
 
 public class MessageService implements Service {
 
@@ -138,74 +132,4 @@ public class MessageService implements Service {
         return "{\"response\":\"ticketrequestfailed\"}";
     }
 
-    public ProtoMessageBuffer.ProtoMessage submitMessageFromProto(ProtoMessageBuffer.ProtoMessage msg) {
-        Message message;
-        int ticketId = msg.getSubmitInstantMessageRequest().getTicketId();
-        String ticketType = msg.getSubmitInstantMessageRequest().getTicketType();
-        String body = msg.getSubmitInstantMessageRequest().getBody();
-        String timestamp = msg.getSubmitInstantMessageRequest().getTimestamp();
-        int authorId = msg.getSubmitInstantMessageRequest().getAuthorId();
-
-        message = MessageRepository.submit(ticketId, ticketType, body, timestamp, authorId);
-
-        if (message == null) return generateFailedResponseProto();
-
-        ProtoMessageBuffer.ProtoMessage response = generateSuccessMessageResponseProto(message);
-        if (!response.isInitialized()) return generateFailedResponseProto();
-
-        return response;
-    }
-
-    private ProtoMessageBuffer.ProtoMessage generateFailedResponseProto() {
-        ProtoMessageBuffer.ProtoMessage.Builder failedResponse = ProtoMessageBuffer.ProtoMessage.newBuilder();
-        failedResponse.setCommand("failedinstantmessage");
-
-        return failedResponse.build();
-    }
-
-    private ProtoMessageBuffer.ProtoMessage generateSuccessMessageResponseProto(Message message) {
-        if (message != null) {
-            ProtoMessageBuffer.ProtoMessage.Builder successMessageResponse = ProtoMessageBuffer.ProtoMessage.newBuilder()
-                    .setCommand("incomingmessage")
-                    .setMessageSubmitRequestAccepted(ProtoMessageBuffer.MessageSubmitRequestAccepted.newBuilder()
-                            .setTicketId(message.getTicketId())
-                            .setAuthorId(message.getAuthorId())
-                            .setTicketType(message.getTicketType())
-                            .setBody(message.getMessage())
-                            .build());
-
-            return successMessageResponse.build();
-        } else return null;
-    }
-
-    public byte[] submitMessageFromJavaObject(byte[] messageRequest) {
-        SubmitMessageRequest submitMessageRequest;
-        Message message;
-
-        submitMessageRequest = (SubmitMessageRequest) ObjectDeserializer.deserialize(messageRequest);
-
-        if (submitMessageRequest != null) {
-            byte[] successResponse;
-
-            int ticketId = submitMessageRequest.getTicketId();
-            String ticketType = submitMessageRequest.getTicketType();
-            String body = submitMessageRequest.getMessageBody();
-            String timestamp = submitMessageRequest.getTimestamp();
-            int authorId = submitMessageRequest.getAuthorId();
-            message = MessageRepository.submit(ticketId, ticketType, body, timestamp, authorId);
-
-            successResponse = ObjectSerializer.serialize(generateSuccessMessageResponseForJavaObject(message));
-
-            return successResponse;
-        } else return generateFailedResponseObject();
-    }
-
-    private byte[] generateFailedResponseObject() {
-        return ObjectSerializer.serialize(new MessageSubmitRequestDenied());
-    }
-
-    private ObjectCommand generateSuccessMessageResponseForJavaObject(Message message) {
-        return new javaobject.MessageSubmitRequestAccepted(message.getTicketId(), message.getTicketType(), message.getMessage(),
-                message.getTimestamp(), message.getAuthorId());
-    }
 }
